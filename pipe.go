78537d94d49071e5a50586e9ba9c83ab817e6efb
Mateusz Mikołajczyk <mikolajczyk.mateusz@gmail.com>
2020-10-07 14:52:15 +0200

    fix spawning zombie processes when one of the commands returns an error

--- pipe.go
+++ pipe.go
@@ -9,6 +9,7 @@ import (
 // Command is a os/exec.Commnad wrapper for UNIX pipe
 func Command(stdout *bytes.Buffer, stack ...*exec.Cmd) (err error) {
 	var stderr bytes.Buffer
+
 	pipeStack := make([]*io.PipeWriter, len(stack)-1)
 	i := 0
 	for ; i < len(stack)-1; i++ {
@@ -18,6 +19,7 @@ func Command(stdout *bytes.Buffer, stack ...*exec.Cmd) (err error) {
 		stack[i+1].Stdin = inPipe
 		pipeStack[i] = outPipe
 	}
+
 	stack[i].Stdout = stdout
 	stack[i].Stderr = &stderr
 
@@ -34,12 +36,16 @@ func call(stack []*exec.Cmd, pipes []*io.PipeWriter) (err error) {
 		if err = stack[1].Start(); err != nil {
 			return err
 		}
+
 		defer func() {
+			pipes[0].Close()
 			if err == nil {
-				pipes[0].Close()
 				err = call(stack[1:], pipes[1:])
+			} else {
+				stack[1].Wait()
 			}
 		}()
 	}
+
 	return stack[0].Wait()
 }
